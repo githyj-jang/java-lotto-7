51141ce7e1a2acd23e8d5120b1260710f589941b
githyj-jang <hoo9268@gmail.com>
2024-11-04 10:33:05 +0900

    test: 로또 번호 발행 및 예외 처리 테스트 코드 추가
    - 1~45 사이의 중복되지 않는 6개의 숫자로 구성된 로또 번호 발행 기능 테스트
    - 잘못된 값 입력 시 IllegalArgumentException 발생 여부 테스트
    - [ERROR]로 시작하는 에러 메시지 출력 확인 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,12 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호의_개수가_6개이하이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -21,5 +28,41 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @Test
+    void 로또_번호의_범위가_넘어가면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 15, 23, 34, 42, 48)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호의_범위가_더_낮아지면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 로또_번호의_중복되면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 1, 2, 2, 2, 4)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 무작위_로또_번호_개수_검증() {
+        Lotto lotto = Lotto.generateRandomLotto();
+        assertThat(lotto.getNumbers()).hasSize(6);
+    }
+
+    @Test
+    void 무작위_로또_번호_범위_검증() {
+        Lotto lotto = Lotto.generateRandomLotto();
+        lotto.getNumbers().forEach(number ->
+                assertThat(number).isBetween(1, 45)
+        );
+    }
+
+    @Test
+    void 무작위_로또_번호_중복_검증() {
+        Lotto lotto = Lotto.generateRandomLotto();
+        assertThat(lotto.getNumbers()).doesNotHaveDuplicates();
+    }
 }
